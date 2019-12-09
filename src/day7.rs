cc28d651cc41ef25638cfa76c3514a054c7314c8
Michael Zavarella <mzavarella30@gmail.com>
2019-12-09 15:30:54 -0500

    Add day 7 solution

--- src/day7.rs
+++ src/day7.rs
@@ -1,6 +1,56 @@
+// https://rosettacode.org/wiki/Permutations#Iterative
+pub fn permutations(start: usize, end: usize) -> Permutations {
+    Permutations {
+        idxs: (start..=end).collect(),
+        swaps: vec![0; end - start + 1],
+        i: 0
+    }
+}
+
+pub struct Permutations {
+    idxs: Vec<usize>,
+    swaps: Vec<usize>,
+    i: usize,
+}
+
+impl Iterator for Permutations {
+    type Item = Vec<usize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i > 0 {
+            loop {
+
+                if self.i >= self.swaps.len() {
+                    return None;
+                }
+
+                if self.swaps[self.i] < self.i {
+                    break;
+                }
+
+                self.swaps[self.i] = 0;
+                self.i += 1;
+            }
+
+            self.idxs.swap(self.i, (self.i & 1) * self.swaps[self.i]);
+            self.swaps[self.i] += 1;
+        }
+        self.i = 1;
+        Some(self.idxs.clone())
+    }
+}
+
 enum Parameter {
-    Position(i32),
-    Immediate(i32),
+    Position(i64),
+    Immediate(i64),
+}
+
+#[derive(PartialEq)]
+enum Signal {
+    NeedsInput,
+    ProducedOutput,
+    Halt,
+    None,
 }
 
 enum Instruction {
@@ -15,21 +65,41 @@ enum Instruction {
     Halt,
 }
 
-struct TestComputer {
-    memory: Vec<i32>,
-    input: Vec<i32>,
-    output: Option<i32>,
+struct IntCodeComputer {
+    memory: Vec<i64>,
+    input: Vec<i64>,
+    output: Option<i64>,
     instruction_pointer: usize,
 }
 
-impl TestComputer {
-    fn get_word(&mut self) -> i32 {
+impl IntCodeComputer {
+    fn new(mem: Vec<i64>, input: Vec<i64>) -> Self {
+        IntCodeComputer {
+            memory: mem,
+            input: input,
+            output: None,
+            instruction_pointer: 0,
+        }
+    }
+
+    fn load_memory(&mut self, mem: Vec<i64>) -> &mut Self {
+        self.memory = mem;
+        self.instruction_pointer = 0;
+        self
+    }
+
+    fn set_input(&mut self, ip: usize) -> &mut Self {
+        self.instruction_pointer = ip;
+        self
+    }
+
+    fn get_word(&mut self) -> i64 {
         let word = self.memory[self.instruction_pointer];
         self.instruction_pointer += 1;
         word
     }
 
-    fn _get_parameter(&mut self, mode: i32) -> Parameter {
+    fn _get_parameter(&mut self, mode: i64) -> Parameter {
         let word = self.get_word();
         match mode {
             0 => Parameter::Position(word),
@@ -38,30 +108,30 @@ impl TestComputer {
         }
     }
 
-    fn get_param_1(&mut self, op_code: i32) -> Parameter {
+    fn get_param_1(&mut self, op_code: i64) -> Parameter {
         self._get_parameter(op_code % 10)
     }
 
-    fn get_param_2(&mut self, op_code: i32) -> (Parameter, Parameter) {
+    fn get_param_2(&mut self, op_code: i64) -> (Parameter, Parameter) {
         let p1 = self.get_param_1(op_code);
         let p2 = self._get_parameter((op_code / 10) % 10);
         (p1, p2)
     }
 
-    fn get_param_3(&mut self, op_code: i32) -> (Parameter, Parameter, Parameter) {
+    fn get_param_3(&mut self, op_code: i64) -> (Parameter, Parameter, Parameter) {
         let (p1, p2) = self.get_param_2(op_code);
         let p3 = self._get_parameter((op_code / 100) % 10);
         (p1, p2, p3)
     }
 
-    fn unwrap(&self, param: Parameter) -> i32 {
+    fn unwrap(&self, param: Parameter) -> i64 {
         match param {
             Parameter::Immediate(val) => val,
             Parameter::Position(pos) => self.memory[pos as usize],
         }
     }
 
-    fn store_val(&mut self, param: Parameter, val: i32) {
+    fn store_val(&mut self, param: Parameter, val: i64) {
         match param {
             Parameter::Position(out) => {
                 self.memory[out as usize] = val;
@@ -76,6 +146,10 @@ impl TestComputer {
 
     fn jump(&mut self, param: Parameter) {
         self.instruction_pointer = self.unwrap(param) as usize;
+    }
+
+    fn feed_input(&mut self, inp: i64) {
+        self.input.push(inp);
     }
 
     fn get_instruction(&mut self) -> Instruction {
@@ -97,70 +171,152 @@ impl TestComputer {
         }
     }
 
-    fn run(&mut self) -> &mut Self {
-        'program_loop: loop {
-            let inst = self.get_instruction();
-            match inst {
-                Instruction::Add((p_1, p_2, p_3)) => {
-                    let op1 = self.unwrap(p_1);
-                    let op2 = self.unwrap(p_2);
-                    self.store_val(p_3, op1 + op2);
+    fn get_output(&mut self) -> i64 {
+        let ret = self.output.unwrap();
+        self.output = None;
+        ret
+    }
+
+    fn tick(&mut self) -> Signal {
+        let inst = self.get_instruction();
+        match inst {
+            Instruction::Add((p_1, p_2, p_3)) => {
+                let op1 = self.unwrap(p_1);
+                let op2 = self.unwrap(p_2);
+                self.store_val(p_3, op1 + op2);
+                return Signal::None;
+            },
+            Instruction::Mul((p_1, p_2, p_3)) => {
+                let op1 = self.unwrap(p_1);
+                let op2 = self.unwrap(p_2);
+                self.store_val(p_3, op1 * op2);
+                return Signal::None;
+            },
+            Instruction::Input(p_) => {
+                if self.input.is_empty() {
+                    return Signal::NeedsInput;
                 }
-                Instruction::Mul((p_1, p_2, p_3)) => {
-                    let op1 = self.unwrap(p_1);
-                    let op2 = self.unwrap(p_2);
-                    self.store_val(p_3, op1 * op2);
+                let inp = self.input.remove(0);
+                self.store_val(p_, inp);
+                return Signal::None;
+            },
+            Instruction::Output(p_) => {
+                self.emit_output(p_);
+                return Signal::ProducedOutput;
+            },
+            Instruction::JumpIfTrue((p_1, p_2)) => {
+                if self.unwrap(p_1) != 0 {
+                    self.jump(p_2);
                 }
-                Instruction::Input(p_) => {
-                    let inp = self.input.remove(0);
-                    self.store_val(p_, inp);
+                return Signal::None;
+            },
+            Instruction::JumpIfFalse((p_1, p_2)) => {
+                if self.unwrap(p_1) == 0 {
+                    self.jump(p_2);
                 }
-                Instruction::Output(p_) => {
-                    self.emit_output(p_);
-                }
-                Instruction::JumpIfTrue((p_1, p_2)) => {
-                    if self.unwrap(p_1) != 0 {
-                        self.jump(p_2);
-                    }
-                }
-                Instruction::JumpIfFalse((p_1, p_2)) => {
-                    if self.unwrap(p_1) == 0 {
-                        self.jump(p_2);
-                    }
-                }
-                Instruction::LessThan((p_1, p_2, p_3)) => {
-                    let op1 = self.unwrap(p_1);
-                    let op2 = self.unwrap(p_2);
-                    self.store_val(p_3, if op1 < op2 { 1 } else { 0 })
-                }
-                Instruction::Equals((p_1, p_2, p_3)) => {
-                    let op1 = self.unwrap(p_1);
-                    let op2 = self.unwrap(p_2);
-                    self.store_val(p_3, if op1 == op2 { 1 } else { 0 })
-                }
-                Instruction::Halt => break 'program_loop,
-            }
+                return Signal::None;
+            },
+            Instruction::LessThan((p_1, p_2, p_3)) => {
+                let op1 = self.unwrap(p_1);
+                let op2 = self.unwrap(p_2);
+                self.store_val(p_3, if op1 < op2 { 1 } else { 0 });
+                return Signal::None;
+            },
+            Instruction::Equals((p_1, p_2, p_3)) => {
+                let op1 = self.unwrap(p_1);
+                let op2 = self.unwrap(p_2);
+                self.store_val(p_3, if op1 == op2 { 1 } else { 0 });
+                return Signal::None;
+            },
+            Instruction::Halt => return Signal::Halt,
         }
-        self
+    }
+
+    fn run_till_signal(&mut self, signal: Signal) {
+        while self.tick() != signal {}
+    }
+
+    fn run(&mut self) -> Signal {
+        let mut signal = self.tick();
+        while signal == Signal::None {
+            signal = self.tick();
+        }
+        signal
     }
 }
 
+fn get_signal(instructions: &Vec<i64>, phase: Vec<usize>) -> i64 {
+    let mut amplifiers = vec![
+        IntCodeComputer::new(instructions.clone(), vec![phase[0] as i64]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[1] as i64]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[2] as i64]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[3] as i64]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[4] as i64]),
+    ];
+
+    let mut out = 0;
+    for i in 0..5 {
+        amplifiers[i].feed_input(out);
+        amplifiers[i].run_till_signal(Signal::ProducedOutput);
+        out = amplifiers[i].get_output();
+    }
+
+    out
+}
+
+fn get_signal_with_feedback(instructions: &Vec<i64>, phase: Vec<usize>) -> i64 {
+    let mut amplifiers = vec![
+        IntCodeComputer::new(instructions.clone(), vec![phase[0] as i64, 0]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[1] as i64]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[2] as i64]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[3] as i64]),
+        IntCodeComputer::new(instructions.clone(), vec![phase[4] as i64]),
+    ];
+
+    let mut sig: Option<i64> = None;
+    for i in (0..5).cycle() {
+        if let Some(s) = sig {
+            amplifiers[i].feed_input(s);
+        }
+
+        match amplifiers[i].run() {
+            Signal::ProducedOutput => {
+                sig = Some(amplifiers[i].get_output());
+            }
+            Signal::Halt => {
+                if i == 4 {
+                    break;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    sig.unwrap()
+}
+
 #[aoc_generator(day7)]
-fn parse_input(input: &str) -> Vec<i32> {
+fn parse_input(input: &str) -> Vec<i64> {
     input
         .split(",")
-        .filter_map(|x| x.parse::<i32>().ok())
+        .filter_map(|x| x.parse::<i64>().ok())
         .collect()
 }
 
 #[aoc(day7, part1)]
-fn solve_p1(instructions: &Vec<i32>) -> Option<i32> {
-    println!("Input: {:?}", instructions);
-    Some(0)
+fn solve_p1(instructions: &Vec<i64>) -> Option<i64> {
+    let mut mx = std::i64::MIN;
+    for perm in permutations(0, 4) {
+        mx = ::std::cmp::max(mx, get_signal(&instructions, perm));
+    }
+    Some(mx)
 }
 
 #[aoc(day7, part2)]
-fn solve_p2(instructions: &Vec<i32>) -> Option<i32> {
-    println!("Input: {:?}", instructions);
-    Some(0)
+fn solve_p2(instructions: &Vec<i64>) -> Option<i64> {
+    let mut mx = std::i64::MIN;
+    for perm in permutations(5, 9) {
+        mx = ::std::cmp::max(mx, get_signal_with_feedback(&instructions, perm));
+    }
+    Some(mx)
 }
